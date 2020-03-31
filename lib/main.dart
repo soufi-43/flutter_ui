2532c24dd353784fd115e71a8359b9ce5fcf4fd0
belhamra soufyane <soufi-43@hotmail.fr>
2020-03-31 19:53:52 +0100

    ADD profile2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutterui/profiles/profile1/profile_1.dart';
+import 'package:flutterui/profiles/profile2/profile2.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home:Profile1(),
+      home:Profile2(),
     );
   }
 }
