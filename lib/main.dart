c1598634ddf8c6734f5438e0483da45c6bddfe47
belhamra soufyane <soufi-43@hotmail.fr>
2020-04-08 23:09:02 +0100

    ADD Registration3

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutterui/profiles/profile4/profile4.dart';
 import 'package:flutterui/profiles/profile5/profile5.dart';
 import 'package:flutterui/registration/registration1.dart';
 import 'package:flutterui/registration/registration2.dart';
+import 'package:flutterui/registration/registration3.dart';
 
 import 'chat/chat2/chat_screen.dart';
 
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home:Registration2(),
+      home:Registration3(),
     );
   }
 }
